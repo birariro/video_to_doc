c128a757c96b613b633cd012729a41c40cea1774
Jinho Lee <vps32@naver.com>
2024-12-13 13:28:50 +0900

    feature
    - model select radio
    - readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# video to doc
+
+## use
+
+### env value
+```
+export OPEN_AI_TOKEN=<your api key>
+```
+
+### local run
+```
+python3 -m venv path/to/venv
+source path/to/venv/bin/activate
+pip install -r requirements.txt
+
+streamlit run index.py
+```
--- index.py
+++ index.py
@@ -5,23 +5,33 @@ import os
 from prompt import get_prompt
 from parser import get_video_script
 
-# Streamlit App
 st.title("YouTube Video to Documentation")
 st.write("`YouTube URL`를 입력하세요.")
 
 videoUrl = st.text_input("YouTube URL")
+model_type = st.radio(
+    "Generate model",
+    ["llama-3.1-sonar-small-128k-online", "llama-3.1-sonar-large-128k-online"],
+    index=1,
+    captions=[
+        "$0.2","$1",
+    ],
+)
 
 if st.button("생성하기"):
+
+    video_script = get_video_script(videoUrl)
+    apiKey = os.getenv('OPEN_AI_TOKEN', 'none')
+    if apiKey == 'none':
+        st.error("none api key")
+        st.stop() 
+    if len(video_script) > 40000:
+        st.error("너무 긴 영상은 불가")
+        st.stop() 
+
     with st.spinner("문서를 생성 중입니다..."):
         try:
-            video_script = get_video_script(videoUrl)
-            apiKey = os.getenv('OPEN_AI_TOKEN', 'abc')
-       
-            if len(video_script) > 40000:
-                st.error("너무 긴 영상은 불가")
-                st.stop()  # 중단
-
-            # OpenAI API interaction
+        
             client = OpenAI(api_key=apiKey, base_url="https://api.perplexity.ai")
             messages = [
                 {
@@ -37,11 +47,10 @@ if st.button("생성하기"):
             ]
 
             response = client.chat.completions.create(
-                model="llama-3.1-sonar-large-128k-online",
+                model=model_type,
                 messages=messages,
             )
 
-            # Display the generated document
             generated_document = response.choices[0].message.content
             st.markdown(generated_document)
 
@@ -53,3 +62,4 @@ if st.button("생성하기"):
             )
         except Exception as e:
             st.error(f"오류가 발생했습니다: {e}")
+
